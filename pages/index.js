7c61f48328152fa8b9e2d31506f83e45c1e2a3d4
abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2022-07-12 20:42:38 +0430

    result component responsived with large screen size

--- pages/index.js
+++ pages/index.js
@@ -10,17 +10,17 @@ export default function Home() {
         <link rel="icon" href="/favicon.png" />
       </Head>  
     <div className='flex justify-center items-center mt-16'>
-        <Image className='' src='/static/images/logo.svg' alt='Tip Calculator' width={80} height={80} ></Image>
+        <Image className='' src='/static/images/logo.svg' alt='Tip Calculator' width={50} height={50} ></Image>
     </div>
 
     {/* start of form */}
-    <div className=' bg-white p-4  my-2 rounded-md md:mx-28 lg:mx-56 xl:mx-96'>
+    <div className=' bg-white p-4 my-2 rounded-md md:mx-28 lg:mx-56 xl:mx-96'>
       <div className=''>
         <form>
-          <span className='text-xl '>Bill</span>
+          <span className='text-md'>Bill</span>
           <input type='text' className='mt-1 w-full p-2 outline-teal-500 bg-cyan-100 rounded-md mb-6' placeholder='$'></input>
-          <span className='text-xl mb-4'>Select Tip %</span>
-          <div className='grid grid-cols-2 gap-2 md:grid-cols-3 mt-2'>
+          <span className='text-md mb-4'>Select Tip %</span>
+          <div className='grid grid-cols-2 gap-2 md:grid-cols-3 mt-2 lg:text-md xl:text-xl'>
             <button className='p-2 bg-teal-700 rounded-md text-white px-4 py-2 font-mono hover:bg-teal-500' type='button'>5 %</button>
             <button className='p-2 bg-teal-700 rounded-md text-white px-4 py-2 font-mono hover:bg-teal-500' type='button'>10 %</button>
             <button className='p-2 bg-teal-700 rounded-md text-white px-4 py-2 font-mono hover:bg-teal-500' type='button'>15 %</button>
@@ -28,10 +28,37 @@ export default function Home() {
             <button className='p-2 bg-teal-700 rounded-md text-white px-4 py-2 font-mono hover:bg-teal-500' type='button'>50 %</button>
             <input type='text' className='outline-teal-500 bg-cyan-100 p-1 w-full h-10 rounded-md placeholder:text-xs placeholder:text-center' placeholder='Custom'></input>
           </div>
+          <div className='mt-6'>
+            <span className='mt-4'>Number of People</span>
+            <input type='text' className='mt-1 w-full p-2 outline-teal-500 bg-cyan-100 rounded-md mb-6' placeholder='$'></input>
+          </div>
         </form>
       </div>
       <div>
       
+      {/* result component  */}
+
+        <div className='bg-teal-800 text-white p-6'>
+            <div>
+              <div className='flex flex-row justify-between'>
+                <p>Tip Amount</p>
+                <span>$0.00</span>
+              </div>
+              <div className='text-xs opacity-70'>
+                <span>/  person</span>
+              </div>
+            </div>
+
+            <div className='flex flex-row justify-between mt-8'>
+                <p>Total</p>
+                <span>$0.00</span>
+              </div>
+              <div className='text-xs opacity-70'>
+                <span>/  person</span>
+              </div>
+            </div>
+
+            
       </div>
     </div>
     </div>
